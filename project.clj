ba30c72e12738c4288a865b5669c503d21936a89
Kevin <kevin.downey@sonian.net>
2010-06-23 09:41:38 -0700

    init commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject graft "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [ring "0.2.3"]])
---
+++ test/graft/core_test.clj
@@ -0,0 +1,6 @@
+(ns graft.core-test
+  (:use [graft.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
