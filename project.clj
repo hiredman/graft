5b60b3f3fd475a2cdbfb7ce9f0011e3c4ebc28c6
Kevin <kevin.downey@sonian.net>
2010-06-23 14:37:53 -0700

    need swank for dev

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [ring "0.2.3"]])
+                 [ring "0.2.3"]]
+  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]])
