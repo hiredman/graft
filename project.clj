969dadd596242c49001daba556462a1efdf42df3
Stephen C. Gilardi <scgilardi@gmail.com>
2011-06-30 05:01:36 +0000

    update to some current versions, rely on plugin install for swank

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject graft "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [ring "0.2.3"]]
-  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [ring "0.3.10"]])
